a80d2ce683f694a6696816bec81c6633f25fe4a3
Aleksandr Krivosheev <77s.a.n.e.k.77@gmail.com>
2023-02-24 14:48:09 +0600

    Add pagination, add parameterized queries

--- Recipe_project/recipe/urls.py
+++ Recipe_project/recipe/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import index, recipes, recipe_dish
 
 urlpatterns = [
-    path('', index),
-    path('recipes', recipes),
-    path('recipe_dish', recipe_dish),
+    path('', index, name='index'),
+    path('recipes', recipes, name='recipes'),
+    path('recipe_dish/<int:dish_id>/', recipe_dish, name='recipe_dish'),
 ]
--- Recipe_project/recipe/views.py
+++ Recipe_project/recipe/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .models import Recipe_dish
 
@@ -11,11 +12,18 @@ def index(request):
 
 def recipes(request):
     list_dish = Recipe_dish.objects.order_by('-published')
-    context = {'list_dish': list_dish}
+    paginator = Paginator(list_dish, 2)
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+    page = paginator.get_page(page_num)
+    context = {'list_dish': page.object_list, 'page': page,
+               'range': range(1, page.paginator.num_pages+1)}
     return render(request, 'recipe/recipes.html', context)
 
 
-def recipe_dish(request):
-    list_dish = Recipe_dish.objects.order_by('-published')
-    context = {'list_dish': list_dish[0]}
+def recipe_dish(request, dish_id):
+    recipe_dish = Recipe_dish.objects.get(id=dish_id)
+    context = {'recipe_dish': recipe_dish}
     return render(request, 'recipe/recipe_dish.html', context)
