38c4f5e0490d85e7feed76644527438c034a1fcf
Aleksandr Krivosheev <77s.a.n.e.k.77@gmail.com>
2023-03-08 09:27:28 +0900

    Перевел страницу индекс на контроллер-класс

--- Recipe_project/recipe/urls.py
+++ Recipe_project/recipe/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import index, RecipeDetailView, RecipesListView
+from .views import RecipeDetailView, RecipesListView, NewAddRecipeList
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', NewAddRecipeList.as_view(), name='index'),
     path('recipes', RecipesListView.as_view(), name='recipes'),
     path('recipe_dish/<int:pk>/',
          RecipeDetailView.as_view(), name='recipe_dish'),
--- Recipe_project/recipe/views.py
+++ Recipe_project/recipe/views.py
@@ -1,28 +1,22 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Recipe_dish
 
 
-def index(request):
-    list_dish = Recipe_dish.objects.order_by('-published')
-    return render(request, 'recipe/index.html',
-                  {'list_dish': list_dish[:3]})
+class NewAddRecipeList(ListView):
+    """
+    Контроллер-класс для представления на страртовом экране списка из трех
+    последних добавленных рецептов
+    """
+    template_name = 'recipe/index.html'
+    model = Recipe_dish
+    paginate_by = 3
+    ordering = '-published'
 
-
-def recipes(request):
-    list_dish = Recipe_dish.objects.order_by('-published')
-    paginator = Paginator(list_dish, 2)
-    if 'page' in request.GET:
-        page_num = request.GET['page']
-    else:
-        page_num = 1
-    page = paginator.get_page(page_num)
-    context = {'list_dish': page.object_list, 'page': page,
-               'range': range(1, page.paginator.num_pages+1)}
-    return render(request, 'recipe/recipes.html', context)
+    def get_context_data(self, **kwargs):
+        contex = super().get_context_data(**kwargs)
+        return contex
 
 
 class RecipesListView(ListView):
